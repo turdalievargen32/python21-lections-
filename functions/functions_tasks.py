c2dc68ba52abfaf8326768d1a27653860cd95ef7
Argen <turdalievargen@gmail.com>
2022-06-21 12:48:40 +0600

    functions.py

---
+++ functions/functions_tasks.py
@@ -0,0 +1,23 @@
+#task1 
+def add(a,b):
+    return a+b
+res = add(4,5)
+print(res)
+
+# task2
+def get_string_length(string):
+   something = len(string)
+   return something 
+
+   #task3
+def get_type(a,b):
+    print(type(a))
+    print(type(b))
+res = get_type(5,'a')
+
+#task4
+def divide(a,b):
+    return a/b
+print(divide(10,2))
+
+
